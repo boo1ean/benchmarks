66577e0d2f19bd46d2a2c6d7fbb5aaf4d3901ad2
boo1ean <boo1ean0807@gmail.com>
2012-12-17 08:02:41 +0200

    Improve benchmark base class, add description feature

--- php/lib/Benchmark.php
+++ php/lib/Benchmark.php
@@ -2,6 +2,21 @@
 abstract class Benchmark
 {
     /**
+     * Elapsed time for current test execution.
+     */
+    protected $elapsed;
+
+    /**
+     * Description of test.
+     */
+    protected $description = 'Unknown test';
+
+    /**
+     * Description output format.
+     */
+    protected $outputFormat = 'Passed :elapsed seconds';
+
+    /**
      * Code to test.
      */
     abstract public function run();
@@ -13,6 +28,15 @@ abstract class Benchmark
     public function execute() {
         $start = microtime(true);
         $this->run();
-        return microtime(true) - $start;
+        $this->elapsed = microtime(true) - $start;
+    }
+
+    /**
+     * View benchmark results (echo to stdout).
+     */
+    public function describe() {
+        echo $this->description . "\n";
+        echo str_replace(':elapsed', $this->elapsed, $this->outputFormat);
+        echo "\n";
     }
 }
